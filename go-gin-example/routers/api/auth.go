40a2a025b3242c7927fbe7249fa5814646e3ec7a
gmx <1721030720@qq.com>
2023-09-26 22:40:28 +0800

    🥞 鉴权和上传的逻辑抽离封装

--- go-gin-example/routers/api/auth.go
+++ go-gin-example/routers/api/auth.go
@@ -1,15 +1,15 @@
 package api
 
 import (
-	"github.com/EGGYC/go-gin-example/pkg/logging"
 	"net/http"
 
 	"github.com/astaxie/beego/validation"
 	"github.com/gin-gonic/gin"
 
-	"github.com/EGGYC/go-gin-example/models"
+	"github.com/EGGYC/go-gin-example/pkg/app"
 	"github.com/EGGYC/go-gin-example/pkg/e"
 	"github.com/EGGYC/go-gin-example/pkg/util"
+	"github.com/EGGYC/go-gin-example/service/auth_service"
 )
 
 type auth struct {
@@ -17,40 +17,144 @@ type auth struct {
 	Password string `valid:"Required; MaxSize(50)"`
 }
 
+// @Summary Get Auth
+// @Produce  json
+// @Param username query string true "userName"
+// @Param password query string true "password"
+// @Success 200 {object} app.Response
+// @Failure 500 {object} app.Response
+// @Router /auth [get]
 func GetAuth(c *gin.Context) {
-	username := c.Query("username")
-	password := c.Query("password")
-
+	appG := app.Gin{C: c}
 	valid := validation.Validation{}
+
+	username := c.PostForm("username")
+	password := c.PostForm("password")
+
 	a := auth{Username: username, Password: password}
 	ok, _ := valid.Valid(&a)
 
-	data := make(map[string]interface{})
-	code := e.INVALID_PARAMS
-	if ok {
-		isExist := models.CheckAuth(username, password) // 转向models数据库的auth.go查询数据库中有无此用户
-		if isExist {
-			token, err := util.GenerateToken(username, password) // pkg/util/jwt.go中的GenerateToken函数
-			if err != nil {
-				code = e.ERROR_AUTH_TOKEN
-			} else {
-				data["token"] = token
-
-				code = e.SUCCESS
-			}
-
-		} else {
-			code = e.ERROR_AUTH
-		}
-	} else {
-		for _, err := range valid.Errors {
-			logging.Info(err.Key, err.Message)
-		}
+	if !ok {
+		app.MarkErrors(valid.Errors)
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
 	}
 
-	c.JSON(http.StatusOK, gin.H{
-		"code": code,
-		"msg":  e.GetMsg(code),
-		"data": data,
+	authService := auth_service.Auth{Username: username, Password: password}
+	isExist, err := authService.Check()
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_CHECK_TOKEN_FAIL, nil)
+		return
+	}
+
+	if !isExist {
+		appG.Response(http.StatusUnauthorized, e.ERROR_AUTH, nil)
+		return
+	}
+
+	token, err := util.GenerateToken(username, password)
+	if err != nil {
+		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_TOKEN, nil)
+		return
+	}
+
+	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
+		"token": token,
 	})
 }
+
+//
+//type auth struct {
+//	Username string `valid:"Required; MaxSize(50)"`
+//	Password string `valid:"Required; MaxSize(50)"`
+//}
+//
+//// @Summary Get Auth
+//// @Produce  json
+//// @Param username query string true "userName"
+//// @Param password query string true "password"
+//// @Success 200 {object} app.Response
+//// @Failure 500 {object} app.Response
+//// @Router /auth [get]
+//func GetAuth(c *gin.Context) {
+//	appG := app.Gin{C: c}
+//	valid := validation.Validation{}
+//
+//	username := c.PostForm("username")
+//	password := c.PostForm("password")
+//
+//	a := auth{Username: username, Password: password}
+//	ok, _ := valid.Valid(&a)
+//
+//	if !ok {
+//		app.MarkErrors(valid.Errors)
+//		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+//		return
+//	}
+//
+//	authService := auth_service.Auth{Username: username, Password: password}
+//	isExist, err := authService.Check()
+//	if err != nil {
+//		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_CHECK_TOKEN_FAIL, nil)
+//		return
+//	}
+//
+//	if !isExist {
+//		appG.Response(http.StatusUnauthorized, e.ERROR_AUTH, nil)
+//		return
+//	}
+//
+//	token, err := util.GenerateToken(username, password)
+//	if err != nil {
+//		appG.Response(http.StatusInternalServerError, e.ERROR_AUTH_TOKEN, nil)
+//		return
+//	}
+//
+//	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
+//		"token": token,
+//	})
+//}
+
+//
+//type auth struct {
+//	Username string `valid:"Required; MaxSize(50)"`
+//	Password string `valid:"Required; MaxSize(50)"`
+//}
+//
+//func GetAuth(c *gin.Context) {
+//	username := c.Query("username")
+//	password := c.Query("password")
+//
+//	valid := validation.Validation{}
+//	a := auth{Username: username, Password: password}
+//	ok, _ := valid.Valid(&a)
+//
+//	data := make(map[string]interface{})
+//	code := e.INVALID_PARAMS
+//	if ok {
+//		isExist := models.CheckAuth(username, password) // 转向models数据库的auth.go查询数据库中有无此用户
+//		if isExist {
+//			token, err := util.GenerateToken(username, password) // pkg/util/jwt.go中的GenerateToken函数
+//			if err != nil {
+//				code = e.ERROR_AUTH_TOKEN
+//			} else {
+//				data["token"] = token
+//
+//				code = e.SUCCESS
+//			}
+//
+//		} else {
+//			code = e.ERROR_AUTH
+//		}
+//	} else {
+//		for _, err := range valid.Errors {
+//			logging.Info(err.Key, err.Message)
+//		}
+//	}
+//
+//	c.JSON(http.StatusOK, gin.H{
+//		"code": code,
+//		"msg":  e.GetMsg(code),
+//		"data": data,
+//	})
+//}
--- go-gin-example/routers/api/upload.go
+++ go-gin-example/routers/api/upload.go
@@ -2,62 +2,60 @@ package api
 
 import (
 	"net/http"
-	"time"
 
 	"github.com/gin-gonic/gin"
 
+	"github.com/EGGYC/go-gin-example/pkg/app"
 	"github.com/EGGYC/go-gin-example/pkg/e"
 	"github.com/EGGYC/go-gin-example/pkg/logging"
 	"github.com/EGGYC/go-gin-example/pkg/upload"
 )
 
-// UploadImage 上传图片
+// @Summary Import Image
+// @Produce  json
+// @Param image formData file true "Image File"
+// @Success 200 {object} app.Response
+// @Failure 500 {object} app.Response
+// @Router /api/v1/tags/import [post]
 func UploadImage(c *gin.Context) {
-	code := e.SUCCESS
-	data := make(map[string]string)
-
-	file, image, err := c.Request.FormFile("image") // c.Request.FormFile：获取上传的图片（返回提供的表单键的第一个文件）
+	appG := app.Gin{C: c}
+	file, image, err := c.Request.FormFile("image")
 	if err != nil {
 		logging.Warn(err)
-		code = e.ERROR
-		c.JSON(http.StatusOK, gin.H{
-			"code": code,
-			"msg":  e.GetMsg(code),
-			"data": data,
-		})
+		appG.Response(http.StatusInternalServerError, e.ERROR, nil)
+		return
 	}
 
 	if image == nil {
-		code = e.INVALID_PARAMS
-	} else {
-		imageName := upload.GetImageName(image.Filename)
-		fullPath := upload.GetImageFullPath()
-		savePath := upload.GetImagePath()
-
-		src := fullPath + imageName
-		if !upload.CheckImageExt(imageName) || !upload.CheckImageSize(file) { // CheckImageExt、CheckImageSize检查图片大小，检查图片后缀
-			code = e.ERROR_UPLOAD_CHECK_IMAGE_FORMAT
-		} else {
-			err := upload.CheckImage(fullPath) // CheckImage：检查上传图片所需（权限、文件夹）
-			if err != nil {
-				logging.Warn(err)
-				code = e.ERROR_UPLOAD_CHECK_IMAGE_FAIL
-			} else if err := c.SaveUploadedFile(image, src); err != nil { // SaveUploadedFile：保存图片
-				logging.Warn(err)
-				code = e.ERROR_UPLOAD_SAVE_IMAGE_FAIL
-			} else {
-				data["image_url"] = upload.GetImageFullUrl(imageName)
-				data["image_save_url"] = savePath + imageName
-			}
-		}
+		appG.Response(http.StatusBadRequest, e.INVALID_PARAMS, nil)
+		return
 	}
 
-	// 延长时间5s  测试 上传过程中 关闭会不会影响它  测试成功 不会影响!
-	time.Sleep(5 * time.Second)
+	imageName := upload.GetImageName(image.Filename)
+	fullPath := upload.GetImageFullPath()
+	savePath := upload.GetImagePath()
+	src := fullPath + imageName
 
-	c.JSON(http.StatusOK, gin.H{
-		"code": code,
-		"msg":  e.GetMsg(code),
-		"data": data,
+	if !upload.CheckImageExt(imageName) || !upload.CheckImageSize(file) {
+		appG.Response(http.StatusBadRequest, e.ERROR_UPLOAD_CHECK_IMAGE_FORMAT, nil)
+		return
+	}
+
+	err = upload.CheckImage(fullPath)
+	if err != nil {
+		logging.Warn(err)
+		appG.Response(http.StatusInternalServerError, e.ERROR_UPLOAD_CHECK_IMAGE_FAIL, nil)
+		return
+	}
+
+	if err := c.SaveUploadedFile(image, src); err != nil {
+		logging.Warn(err)
+		appG.Response(http.StatusInternalServerError, e.ERROR_UPLOAD_SAVE_IMAGE_FAIL, nil)
+		return
+	}
+
+	appG.Response(http.StatusOK, e.SUCCESS, map[string]string{
+		"image_url":      upload.GetImageFullUrl(imageName),
+		"image_save_url": savePath + imageName,
 	})
 }
