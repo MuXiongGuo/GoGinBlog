92c1a98ffefaf4a023d120791de3583bda2eb074
gmx <1721030720@qq.com>
2023-09-26 14:30:14 +0800

    🍩 分页工具包 实现获取分页参数，传入page后根据setting中的设置得到res，res为models中的db查询时offset的数量

---
+++ go-gin-example/pkg/util/pagination.go
@@ -0,0 +1,20 @@
+// Package util 编写工具包
+package util
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/unknwon/com"
+
+	"github.com/EGGYC/go-gin-example/pkg/setting"
+)
+
+// GetPage 分页页码的获取方法
+func GetPage(c *gin.Context) int {
+	result := 0
+	page, _ := com.StrTo(c.Query("page")).Int()
+	if page > 0 {
+		result = (page - 1) * setting.AppSetting.PageSize
+	}
+
+	return result
+}
